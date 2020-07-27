2c2e799b12f4545d4b76cf24d003080dc349739e
RishiBaskaran <68693669+RishiBaskaran@users.noreply.github.com>
2020-07-27 12:09:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,29 @@
-var rectangle1,rectangle2;
+var car,wall;
 
 
 function setup() {
   createCanvas(800,400);
-  rectangle1 = createSprite(400, 200, 40, 50);
-  rectangle2 = createSprite(200,200,30,40)
-  rectangle1.shapeColor = "red";
-  rectangle2.shapeColor = "red";
-  rectangle1.velocityX = -3;
+  car = createSprite(400, 200, 40, 50);
+ wall = createSprite(200,200,30,40)
+  car.shapeColor = "white";
+  wall.shapeColor = "red";
+car.velocityX = -3;
  // rectangle1.velocityY = -3;
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  background("black");
  // rectangle2.x = World.mouseX;
   //rectangle2.y = World.mouseY;
 
-if(rectangle1.x - rectangle2.x < rectangle1.width/2 + rectangle2.width/2 && 
-  rectangle2.x - rectangle1.x < rectangle1.width/2 + rectangle2.width/2 &&
-  rectangle1.y - rectangle2.y < rectangle1.height/2 + rectangle2.height/2 &&
-  rectangle2.y - rectangle1.y < rectangle1.height/2 + rectangle2.height/2){
-//rectangle1.shapeColor = "blue";
+if(car.x - wall.x < car.width/2 + wall.width/2 && 
+  wall.x - car.x < car.width/2 + wall.width/2 &&
+car.y - wall.y < wall.height/2 + car.height/2 &&
+  wall.y - car.y < car.height/2 + wall.height/2){
+car.shapeColor = "red";
 //rectangle2.shapeColor = "blue";
-rectangle1.velocityX = rectangle1.velocityX * (-1);
+car.velocityX = car.velocityX * (-1);
 }
 
   drawSprites();
