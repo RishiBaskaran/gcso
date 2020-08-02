65d2b03bd3f4688db94d1a8f4fb9d10164fa8479
RishiBaskaran <68693669+RishiBaskaran@users.noreply.github.com>
2020-08-02 21:35:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var car,wall;
 var speed,weight;
 var deformation;
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1600,400);
   speed = random(55,90);
   weight = random(400,1500);
   car = createSprite(400, 200, 100, 50);
