f903bc0b8e766ddb50cafb190092d2843a6332d2
RishiBaskaran <68693669+RishiBaskaran@users.noreply.github.com>
2020-08-04 12:51:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,7 @@ function setup() {
   createCanvas(1600,400);
   speed = random(55,90);
   weight = random(400,1500);
-  car = createSprite(400, 200, 100, 50);
- wall = createSprite(200,200,20,40)
+ 
   car.shapeColor ="white";
   wall.shapeColor = color(80,80,80);
  
@@ -17,19 +16,8 @@ function setup() {
 function draw() {
   background(255,255,255);
   background("black");
- // rectangle2.x = World.mouseX;
-  //rectangle2.y = World.mouseY;
-
-/*if(car.x - wall.x < car.width/2 + wall.width/2 && 
-  wall.x - car.x < car.width/2 + wall.width/2 &&
-car.y - wall.y < wall.height/2 + car.height/2 &&
-  wall.y - car.y < car.height/2 + wall.height/2){
-car.shapeColor = "blue";
-//rectangle2.shapeColor = "blue";
-//car.velocityX = car.velocityX * (-1);
-  car.velocityX = 0;
-}
- */
+  car = createSprite(50, 200, 50, 50);
+ wall = createSprite(1200,200,60,40);
   car.velocityX = speed;
   if(wall.x - car.x < (car.width + wall.width)/2){
     car.velocityX = 0;
